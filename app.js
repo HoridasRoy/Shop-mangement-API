83044a71c0ac30bf625db13c1acbe9227ef23811
Horidas Roy <horidasr@gmail.com>
2018-11-18 00:52:00 +0600

    adding signup operations

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
 const mongoose = require('mongoose');
 
 /*mongoose.connect('mongodb://node-shop:'+ process.env.MONGO_ATLAS_PW +'@node-rest-shop-shard-00-00-pck5b.mongodb.net:27017,node-rest-shop-shard-00-01-pck5b.mongodb.net:27017,node-rest-shop-shard-00-02-pck5b.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin&retryWrites=true:',{
@@ -36,6 +37,7 @@ app.use((req, res, next) =>{
 //routes which handle request.
 app.use('/products',productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/user', userRoutes);
 
 app.use((req, res, next) => {
   const error = new Error('Not found');
